ca1acccc515dfb8549ee3310a78e0bef8b955dfa
S0o0 <S0o0@users.noreply.github.com>
2025-09-26 21:36:35 +0200

    text align left

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,19 @@ import './App.css'
 import { tasksCollection } from './data'
 
 function App() {
-  const tasks = tasksCollection;
+  const tasks = tasksCollection; // pas sûr s'il faut utiliser useStat à la place
   return (
     <>
       <h1>Todo List</h1>
       <p>Nombre de tâches : {tasks.length}</p>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {task.content}
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
-
 export default App
